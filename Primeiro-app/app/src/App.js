fcd24194ef2fefdea132d03cf36612035f1463e3
victorikellmy <victorikellmyferreira123@gmail.com>
2023-03-09 16:58:46 -0300

    Primeiro app

--- Primeiro-app/app/src/App.js
+++ Primeiro-app/app/src/App.js
@@ -1,7 +1,12 @@
+import Nomes from "./components/Nomes";
+
 function App() {
   return (
     <div>
       <h1>Bem vindo ao meu projeto </h1>
+      <Nomes nome="joao" idade={10}/>
+      <br/>
+      <Nomes nome="pedro" idade={12}/>
     </div>
   );
 }
---
+++ Primeiro-app/app/src/components/Nomes.js
@@ -0,0 +1,10 @@
+function Nomes(props){
+    return(
+
+        <h2>
+            Aluno: {props.nome} {props.idade} anos
+        </h2> 
+
+    );
+}
+export default Nomes;
